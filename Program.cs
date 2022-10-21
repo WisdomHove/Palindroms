48efe0a122c3601880bb60ab9ad5d9c301465e2e
wisdomhove <hovewisdom@gmail.com>
2022-10-21 08:39:19 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Palindroms{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            Console.WriteLine("Enter a word: ");
+            string str = Console.ReadLine();
+
+            string temp = "";
+
+            for(int i = str.Length-1; i >= 0; i--){
+
+                temp += str[i];
+            }
+
+            if(temp == str){
+
+                Console.WriteLine(str+" is a palindrom.");
+            } else{
+
+                Console.WriteLine(str+" is not a palindrom.");
+            }
+        }
+    }
+}
